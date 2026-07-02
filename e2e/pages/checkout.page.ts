bf113d4d68ae003a73e73e451651cf750fad52c1
Romario Nijim <rn@Romarios-MacBook-Pro.local>
2026-07-02 16:39:49 +0300

    chore: remove commented out code

--- e2e/pages/checkout.page.ts
+++ e2e/pages/checkout.page.ts
@@ -2,18 +2,7 @@ import { CheckoutDetails } from "../types/checkout-details.type";
 import { BasePage } from "./base.page";
 
 export class CheckoutPage extends BasePage {
-    // firstNameField = this.page.getByTestId('firstName');
-    // lastNameField = this.page.getByTestId('lastName');
-    // zipCodeField = this.page.getByTestId('postalCode');
     continueButton = this.page.getByTestId('continue');
-
-
-    // async fillCheckoutDetails(checkoutDetails: CheckoutDetails) {
-    //     await this.firstNameField.fill(checkoutDetails.firstName);
-    //     await this.lastNameField.fill(checkoutDetails.lastName);
-    //     await this.zipCodeField.fill(checkoutDetails.postalCode.toString());
-    //     await this.continueButton.click();
-    // }
 
     async fillCheckoutDetails(checkoutDetails: CheckoutDetails) {
         const fieldNameLocators: (keyof CheckoutDetails)[] = ['firstName', 'lastName', 'postalCode'];
