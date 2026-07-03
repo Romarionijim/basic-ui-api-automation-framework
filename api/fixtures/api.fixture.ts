514e18515f42f6ec7af7fe99acaae1e5303b98a8
Romario Nijim <rn@Romarios-MacBook-Pro.local>
2026-07-03 10:51:00 +0300

    chore(api): rename resources to services in fixtures

--- api/fixtures/api.fixture.ts
+++ api/fixtures/api.fixture.ts
@@ -1,11 +1,11 @@
 import { PostsService } from "../services/posts/post.service"
 import { test as base } from '@playwright/test';
 
-export type ApiResources = {
+export type ApiServices = {
     posts: PostsService
 }
 
-export const test = base.extend<ApiResources>({
+export const test = base.extend<ApiServices>({
     posts: async ({ request }, use) => {
         await use(new PostsService(request))
     }
