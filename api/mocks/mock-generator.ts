65e1d18082aa807b7e9e997e88a613bed845da28
Romario Nijim <rn@Romarios-MacBook-Pro.local>
2026-07-18 12:11:48 +0300

    feat(api): use word instead of prefix mock

--- api/mocks/mock-generator.ts
+++ api/mocks/mock-generator.ts
@@ -7,7 +7,7 @@ class MockGenerator extends Chance {
 
     generatePost() {
         return {
-            title: this.name_prefix(),
+            title: this.word(),
             body: this.sentence({ words: 5 }),
             userId: this.integer({ min: 1, max: 1000 }),
             id: this.integer({ min: 1, max: 10000 })
